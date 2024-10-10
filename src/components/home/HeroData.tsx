01c85dda704a8085c9040e8a409f522ad6b6fae2
hlakarki <kshap.karki@gmail.com>
2024-10-10 10:26:48 -0400

    feat: Added BriefStats component to HeroData for displaying counters' statistics and replaced static sections with Tabs for counters and compatibilities.

--- src/components/home/HeroData.tsx
+++ src/components/home/HeroData.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Sword, Shield, Users, TrendingUp, TrendingDown, MapPin, Tag } from 'lucide-react'
 import { HeroDetails as HeroDetailsType, HeroInfo } from "@/lib/types"
 import dataJSON from "@/lib/data/ids.json"
+import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/ui/tabs'
 
 interface CombinedHeroProps {
   details: HeroDetailsType;
@@ -12,6 +13,40 @@ interface CombinedHeroProps {
 export function HeroData({ details, info }: CombinedHeroProps) {
   const { counters, compatibilities } = details;
 
+
+  const BriefStats = () => {
+    return (
+        <>
+          <div className="flex items-center justify-between mb-4">
+            <Image
+                src={counters.head}
+                alt={counters.name}
+                width={60}
+                height={60}
+                className="rounded-full"
+            />
+            <div className="text-right">
+              <p className="font-semibold">{counters.name}</p>
+              <p className="text-sm text-blue-200">Rank: {counters.rank}</p>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4 mb-4">
+            <div>
+              <p className="text-sm text-blue-200">Appearance Rate</p>
+              <p className="font-semibold">{(counters.appearance_rate * 100).toFixed(2)}%</p>
+            </div>
+            <div>
+              <p className="text-sm text-blue-200">Ban Rate</p>
+              <p className="font-semibold">{(counters.ban_rate * 100).toFixed(2)}%</p>
+            </div>
+            <div>
+              <p className="text-sm text-blue-200">Win Rate</p>
+              <p className="font-semibold">{(counters.win_rate * 100).toFixed(2)}%</p>
+            </div>
+          </div>
+        </>
+    )
+  }
   return (
       <div className="bg-blue-800 bg-opacity-50 rounded-lg p-6 mt-8 shadow-lg">
         <div className="flex flex-col md:flex-row items-center md:items-start gap-6 mb-8">
@@ -28,8 +63,8 @@ export function HeroData({ details, info }: CombinedHeroProps) {
               {info.type.map((type, index) => {
                 if (type && type !== "") {
                   return (
-                    <span key={index} className="bg-blue-600 text-white px-2 py-1 rounded text-sm flex items-center">
-                    <Tag className="w-4 h-4 mr-1" />
+                      <span key={index} className="bg-blue-600 text-white px-2 py-1 rounded text-sm flex items-center">
+                    <Tag className="w-4 h-4 mr-1"/>
                         {type}
                     </span>
                   )
@@ -40,7 +75,7 @@ export function HeroData({ details, info }: CombinedHeroProps) {
               {info.lane.map((lane, index) => {
                 if (lane && lane !== "") {
                   return (
-                    <span key={index} className="bg-green-600 text-white px-2 py-1 rounded text-sm flex items-center">
+                      <span key={index} className="bg-green-600 text-white px-2 py-1 rounded text-sm flex items-center">
                     <MapPin className="w-4 h-4 mr-1"/>
                           {lane}
                     </span>
@@ -61,141 +96,117 @@ export function HeroData({ details, info }: CombinedHeroProps) {
           </div>
         </div>
 
+        <BriefStats />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <h2 className="text-2xl font-bold mb-4 flex items-center">
-              <Sword className="mr-2" /> Counters
+              <Sword className="mr-2"/> Counters
             </h2>
-            <div className="bg-blue-700 bg-opacity-50 p-4 rounded-lg">
-              <div className="flex items-center justify-between mb-4">
-                <Image
-                    src={counters.head}
-                    alt={counters.name}
-                    width={60}
-                    height={60}
-                    className="rounded-full"
-                />
-                <div className="text-right">
-                  <p className="font-semibold">{counters.name}</p>
-                  <p className="text-sm text-blue-200">Rank: {counters.rank}</p>
+            <Tabs defaultValue="best-against" className="w-full">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="best-against">Best Against</TabsTrigger>
+                <TabsTrigger value="worst-against">Worst Against</TabsTrigger>
+              </TabsList>
+              <TabsContent value="best-against">
+                <div className="p-4 bg-background rounded-b-lg border border-t-0">
+                  <h3 className="text-lg font-semibold mb-2 flex items-center">
+                    <TrendingUp className="mr-2"/> {counters.name} counters these heroes very well
+                  </h3>
+                  <ul className="space-y-2">
+                    {counters.counters.map((counter, index) => (
+                        <li key={index} className="flex items-center justify-between">
+                          <div className="flex items-center text-black">
+                            <Image src={counter.head} alt={`Hero ${counter.heroid}`} width={30} height={30}
+                                   className="rounded-full mr-2"/>
+                            <span>{getHeroName(counter.heroid)}</span>
+                          </div>
+                          <div className="flex items-center">
+                            <TrendingUp className="w-4 h-4 mr-1 text-green-400"/>
+                            <span className="text-green-400">{(counter.hero_win_rate * 100).toFixed(1)}%</span>
+                          </div>
+                        </li>
+                    ))}
+                  </ul>
                 </div>
-              </div>
-              <div className="grid grid-cols-2 gap-4 mb-4">
-                <div>
-                  <p className="text-sm text-blue-200">Appearance Rate</p>
-                  <p className="font-semibold">{(counters.appearance_rate * 100).toFixed(2)}%</p>
+              </TabsContent>
+              <TabsContent value="worst-against">
+                <div className="p-4 bg-background rounded-b-lg border border-t-0">
+                  <h3 className="text-lg font-semibold mb-2 flex items-center">
+                    <TrendingDown className="mr-2"/> {counters.name} is most countered by these heroes
+                  </h3>
+                  <ul className="space-y-2">
+                    {counters.most_countered_by.map((counter, index) => (
+                        <li key={index} className="flex items-center justify-between">
+                          <div className="flex items-center text-black">
+                            <Image src={counter.head} alt={`Hero ${counter.heroid}`} width={30} height={30}
+                                   className="rounded-full mr-2"/>
+                            <span>{getHeroName(counter.heroid)}</span>
+                          </div>
+                          <div className="flex items-center">
+                            <TrendingDown className="w-4 h-4 mr-1 text-red-400"/>
+                            <span className="text-red-400">{(counter.hero_win_rate * 100).toFixed(1)}%</span>
+                          </div>
+                        </li>
+                    ))}
+                  </ul>
                 </div>
-                <div>
-                  <p className="text-sm text-blue-200">Ban Rate</p>
-                  <p className="font-semibold">{(counters.ban_rate * 100).toFixed(2)}%</p>
-                </div>
-                <div>
-                  <p className="text-sm text-blue-200">Win Rate</p>
-                  <p className="font-semibold">{(counters.win_rate * 100).toFixed(2)}%</p>
-                </div>
-              </div>
-              <h3 className="text-lg font-semibold mb-2 flex items-center">
-                <TrendingUp className="mr-2" /> {counters.name} counters these heroes very well
-              </h3>
-              <ul className="space-y-2 mb-4">
-                {counters.counters.map((counter, index) => (
-                    <li key={index} className="flex items-center justify-between">
-                      <div className="flex items-center">
-                        <Image src={counter.head} alt={`Hero ${counter.heroid}`} width={30} height={30} className="rounded-full mr-2" />
-                        <span>{getHeroName(counter.heroid)}</span>
-                      </div>
-                      <div className="flex items-center">
-                        <TrendingUp className="w-4 h-4 mr-1" />
-                        <span className="text-green-400">{(counter.hero_win_rate * 100).toFixed(1)}%</span>
-                      </div>
-                    </li>
-                ))}
-              </ul>
-              <h3 className="text-lg font-semibold mb-2 flex items-center">
-                <TrendingDown className="mr-2" /> {counters.name} is most countered by these heroes
-              </h3>
-              <ul className="space-y-2">
-                {counters.most_countered_by.map((counter, index) => (
-                    <li key={index} className="flex items-center justify-between">
-                      <div className="flex items-center">
-                        <Image src={counter.head} alt={`Hero ${counter.heroid}`} width={30} height={30} className="rounded-full mr-2" />
-                        <span>{getHeroName(counter.heroid)}</span>
-                      </div>
-                      <div className="flex items-center">
-                        <TrendingDown className="w-4 h-4 mr-1" />
-                        <span className="text-red-400">{(counter.hero_win_rate * 100).toFixed(1)}%</span>
-                      </div>
-                    </li>
-                ))}
-              </ul>
-            </div>
+              </TabsContent>
+            </Tabs>
           </div>
           <div>
             <h2 className="text-2xl font-bold mb-4 flex items-center">
-              <Users className="mr-2" /> Compatibilities
+              <Users className="mr-2"/> Compatibilities
             </h2>
-            <div className="bg-blue-700 bg-opacity-50 p-4 rounded-lg">
-              <div className="flex items-center justify-between mb-4">
-                <Image
-                    src={compatibilities.head}
-                    alt={compatibilities.name}
-                    width={60}
-                    height={60}
-                    className="rounded-full"
-                />
-                <div className="text-right">
-                  <p className="font-semibold">{compatibilities.name}</p>
+            <Tabs defaultValue="best-with" className="w-full">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="best-with">Best With</TabsTrigger>
+                <TabsTrigger value="worst-with">Worst With</TabsTrigger>
+              </TabsList>
+              <TabsContent value="best-with">
+                <div className="p-4 bg-background rounded-b-lg border border-t-0">
+                  <h3 className="text-lg font-semibold mb-2 flex items-center">
+                    <TrendingUp className="mr-2"/> {counters.name} works best with these heroes
+                  </h3>
+                  <ul className="space-y-2">
+                    {compatibilities.most_compatible.map((hero, index) => (
+                        <li key={index} className="flex items-center justify-between">
+                          <div className="flex items-center text-black">
+                            <Image src={hero.head} alt={`Hero ${hero.heroid}`} width={30} height={30}
+                                   className="rounded-full mr-2"/>
+                            <span>{getHeroName(hero.heroid)}</span>
+                          </div>
+                          <div className="flex items-center">
+                            <TrendingUp className="w-4 h-4 mr-1 text-green-400"/>
+                            <span className="text-green-400">{(hero.increase_win_rate * 100).toFixed(1)}%</span>
+                          </div>
+                        </li>
+                    ))}
+                  </ul>
                 </div>
-              </div>
-              <div className="grid grid-cols-2 gap-4 mb-4">
-                <div>
-                  <p className="text-sm text-blue-200">Appearance Rate</p>
-                  <p className="font-semibold">{(compatibilities.appearance_rate * 100).toFixed(2)}%</p>
+              </TabsContent>
+              <TabsContent value="worst-with">
+                <div className="p-4 bg-background rounded-b-lg border border-t-0">
+                  <h3 className="text-lg font-semibold mb-2 flex items-center">
+                    <TrendingDown className="mr-2"/> {counters.name} does not work well with these heroes
+                  </h3>
+                  <ul className="space-y-2">
+                    {compatibilities.least_compatible.map((hero, index) => (
+                        <li key={index} className="flex items-center justify-between">
+                          <div className="flex items-center text-black">
+                            <Image src={hero.head} alt={`Hero ${hero.heroid}`} width={30} height={30}
+                                   className="rounded-full mr-2"/>
+                            <span>{getHeroName(hero.heroid)}</span>
+                          </div>
+                          <div className="flex items-center">
+                            <TrendingDown className="w-4 h-4 mr-1 text-red-400" />
+                            <span className="text-red-400">{(hero.increase_win_rate * 100).toFixed(1)}%</span>
+                          </div>
+                        </li>
+                    ))}
+                  </ul>
                 </div>
-                <div>
-                  <p className="text-sm text-blue-200">Ban Rate</p>
-                  <p className="font-semibold">{(compatibilities.ban_rate * 100).toFixed(2)}%</p>
-                </div>
-                <div>
-                  <p className="text-sm text-blue-200">Win Rate</p>
-                  <p className="font-semibold">{(compatibilities.win_rate * 100).toFixed(2)}%</p>
-                </div>
-              </div>
-              <h3 className="text-lg font-semibold mb-2 flex items-center">
-                <TrendingUp className="mr-2" /> {counters.name} works best with these heroes
-              </h3>
-              <ul className="space-y-2 mb-4">
-                {compatibilities.most_compatible.map((hero, index) => (
-                    <li key={index} className="flex items-center justify-between">
-                      <div className="flex items-center">
-                        <Image src={hero.head} alt={`Hero ${hero.heroid}`} width={30} height={30} className="rounded-full mr-2" />
-                        <span>{getHeroName(hero.heroid)}</span>
-                      </div>
-                      <div className="flex items-center">
-                        <TrendingUp className="w-4 h-4 mr-1" />
-                        <span className="text-green-400">{(hero.increase_win_rate * 100).toFixed(1)}%</span>
-                      </div>
-                    </li>
-                ))}
-              </ul>
-              <h3 className="text-lg font-semibold mb-2 flex items-center">
-                <TrendingDown className="mr-2" /> {counters.name} does not work well with these heroes
-              </h3>
-              <ul className="space-y-2">
-                {compatibilities.least_compatible.map((hero, index) => (
-                    <li key={index} className="flex items-center justify-between">
-                      <div className="flex items-center">
-                        <Image src={hero.head} alt={`Hero ${hero.heroid}`} width={30} height={30} className="rounded-full mr-2" />
-                        <span>{getHeroName(hero.heroid)}</span>
-                      </div>
-                      <div className="flex items-center">
-                        <TrendingDown className="w-4 h-4 mr-1" />
-                        <span className="text-red-400">{(hero.increase_win_rate * 100).toFixed(1)}%</span>
-                      </div>
-                    </li>
-                ))}
-              </ul>
-            </div>
+              </TabsContent>
+            </Tabs>
           </div>
         </div>
 
