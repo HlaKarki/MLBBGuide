ad48f34bef892e6f09fdef83bca931499fef977c
hlakarki <kshap.karki@gmail.com>
2024-10-10 14:30:43 -0400

    style: Enhance header layout by adding logo and navigation links with improved styling and structure for better user experience.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import {Logo} from "@/components/Logo";
+
 export default function Header() {
   return (
-      <header>
-        <div>Ok</div>
+      <header className="text-white p-4">
+        <div className="flex justify-start items-center gap-8">
+          <div className="flex items-center space-x-4">
+            <Logo className={"h-auto w-20"} />
+            <h1 className="absolute invisible">Mobile Legends: Bang Bang Rank Helper Tool</h1>
+          </div>
+          <nav>
+            <ul className="flex space-x-4">
+              <li><Link href="/search" className={""}>Search</Link></li>
+              <li><Link href="/rank-helper" className={""}>Rank Helper</Link></li>
+            </ul>
+          </nav>
+        </div>
       </header>
   )
 }
