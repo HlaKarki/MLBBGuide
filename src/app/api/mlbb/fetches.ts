092752203df4ca4244309f33eb01590293ab76c1
hlakarki <kshap.karki@gmail.com>
2024-10-11 15:30:20 -0400

    feat: Implemented data processing functions for hero attributes and relations in fetches.ts, enhancing the structure for hero information extraction.

--- src/app/api/mlbb/fetches.ts
+++ src/app/api/mlbb/fetches.ts
@@ -1,3 +1,5 @@
+import {HeroInfo, RawDataType} from "@/lib/types";
+
 const baseUrl = process.env.MLBB_API_BASE_URL || "";
 const firstId = process.env.MLBB_FIRST_ID || "/2669606";
 const meta_heroes = process.env.MLBB_SECOND_ID_META_HEROES || "/2756567";
@@ -36,3 +38,42 @@ export async function fetchStats(sortField: string, count: number) {
 
   return await response.json();
 }
+
+const processRelation = (relationData: any) => ({
+  heads: relationData.target_hero
+      .filter((hero: any) => hero && typeof hero === 'object' && hero.data && hero.data.head)
+      .map((hero: any) => hero.data.head),
+  description: relationData.desc
+});
+
+export const processData = (rawData: RawDataType): Record<string, HeroInfo> => {
+  return Object.entries(rawData.data.records).reduce((acc, [key, { data }]) => {
+    const { hero, head, head_big, relation } = data;
+    const { _createdAt, _updatedAt, data: heroData } = hero;
+
+    acc[key] = {
+      name: heroData.name,
+      head,
+      head_big,
+      square_head: heroData.squarehead,
+      square_head_big: heroData.squareheadbig,
+      createdAt: _createdAt,
+      updatedAt: _updatedAt,
+      stats: {
+        Durability: heroData.abilityshow[0] || "0",
+        Offense: heroData.abilityshow[1] || "0",
+        "Ability Effects": heroData.abilityshow[2] || "0",
+        Difficulty: heroData.abilityshow[3] || "0"
+      },
+      lane: heroData.roadsortlabel,
+      speciality: heroData.speciality,
+      title: heroData.story,
+      relation: {
+        "works_well_with": processRelation(relation.assist),
+        "strong_against": processRelation(relation.strong),
+        "weak_against": processRelation(relation.weak)
+      }
+    };
+    return acc;
+  }, {} as Record<string, HeroInfo>);
+};
