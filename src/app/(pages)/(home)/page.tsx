813ca75437783a2235c472fddf1b6ae430b33df6
hlakarki <kshap.karki@gmail.com>
2024-10-11 12:52:32 -0400

    refactor: Remove unused state and fetch logic from Home component, simplifying the code and updating the UI to directly render StatsTemp without fetching stats.

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,25 +1,9 @@
 'use client'
 
-import React, {useCallback, useEffect, useState} from 'react'
+import React from 'react'
 import { motion } from 'framer-motion'
-import {Stats} from "@/components/search/Stats";
-import {StatsType} from "@/lib/types";
-import StatsTemp from "@/app/(pages)/(home)/Stats";
 
 export default function Home() {
-  const [stats, setStats] = useState<StatsType | null>(null)
-  const fetchStats = useCallback(async  () => {
-    const response = await fetch("/api/mlbb/stats")
-    const stats = await response.json()
-
-    if (stats.errors) {return}
-
-    setStats(stats)
-  }, [])
-
-  useEffect(() => {
-      fetchStats().catch((err) => console.error(err))
-  }, []);
 
   return (
       <div className="min-h-screen bg-gradient-to-b from-blue-900 to-blue-950 text-white">
@@ -30,15 +14,6 @@ export default function Home() {
             className="container mx-auto px-4 py-8 max-w-6xl"
         >
           WELCOME
-        </motion.div>
-
-        <motion.div
-            initial={{opacity: 0, y: 50}}
-            animate={{opacity: 1, y: 0}}
-            transition={{duration: 0.5, delay: 0.4}}
-        >
-          {/*<Stats stats={stats}/>*/}
-          <StatsTemp />
         </motion.div>
       </div>
   )
