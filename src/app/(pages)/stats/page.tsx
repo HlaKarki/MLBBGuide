1f9518b6688c3e400b020bade34802f9f019c325
hlakarki <kshap.karki@gmail.com>
2024-10-11 20:56:34 -0400

    refactor: Simplify data fetching in Statistics component by replacing useState and useEffect with useQuery from react-query and update to use StatsTable for rendering.

--- src/app/(pages)/stats/page.tsx
+++ src/app/(pages)/stats/page.tsx
@@ -1,24 +1,22 @@
 'use client'
 
-import React, {useCallback, useEffect, useState} from 'react'
+import React from 'react'
 import { motion } from 'framer-motion'
-import {StatsTable } from "@/lib/types";
-import ImprovedStatsTable from "@/app/(pages)/stats/Stats";
+import { StatsTableType } from "@/lib/types"
+import { useQuery } from '@tanstack/react-query'
+import StatsTable from "@/app/(pages)/stats/Stats";
 
 export default function Statistics() {
-  const [stats, setStats] = useState<StatsTable[] | []>([])
-  const fetchStats = useCallback(async  () => {
-    const response = await fetch("/api/mlbb/stats?hla=1")
-    const stats = await response.json()
-
-    if (stats.errors) {return}
-
-    setStats(stats)
-  }, [])
-
-  useEffect(() => {
-      fetchStats().catch((err) => console.error(err))
-  }, []);
+  const { data: stats, isLoading, error } = useQuery<StatsTableType[], Error>({
+    queryKey: ['stats'],
+    queryFn: async (): Promise<StatsTableType[]> => {
+      const response = await fetch("/api/mlbb/stats?hla=1")
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      return response.json()
+    },
+  })
 
   return (
       <div className="min-h-screen bg-gradient-to-b from-blue-900 to-blue-950 text-white">
@@ -27,8 +25,7 @@ export default function Statistics() {
             animate={{opacity: 1, y: 0}}
             transition={{duration: 0.5, delay: 0.4}}
         >
-          {/*<Stats stats={stats}/>*/}
-          <ImprovedStatsTable stats={stats}/>
+          <StatsTable stats={stats} isLoading={isLoading} error={error} />
         </motion.div>
       </div>
   )
