da76189161777a06b61f1d0e30abf3937e0d13ef
hlakarki <kshap.karki@gmail.com>
2024-10-12 08:21:52 -0400

    fix: Updated fetchGraph function call to include rank parameter for improved data retrieval in GET request.

--- src/app/api/mlbb/graph/route.ts
+++ src/app/api/mlbb/graph/route.ts
@@ -4,9 +4,10 @@ import {fetchGraph} from "@/app/api/mlbb/fetches";
 
 export async function GET(request: NextRequest) {
   const hero_id = request.nextUrl.searchParams.get("id") || "-1"
+  const rank = request.nextUrl.searchParams.get("rank") || "7"
   const period = Number(request.nextUrl.searchParams.get("period")) || 7
   try {
-    const data =  await fetchGraph(hero_id, period).then(res => res.data.records[0]);
+    const data =  await fetchGraph(hero_id, period, rank).then(res => res.data.records[0]);
 
     return NextResponse.json(data);
   } catch (error) {
