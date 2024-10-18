98714f0b50f7c77aee4094da1a71e38585e95e05
hlakarki <kshap.karki@gmail.com>
2024-10-18 14:05:40 -0400

    fix: Update import path for StatsTable component to reflect new directory structure

--- src/app/(pages)/stats/page.tsx
+++ src/app/(pages)/stats/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { StatsTableType } from "@/lib/types"
 import { useQuery } from '@tanstack/react-query'
-import StatsTable from "@/app/(pages)/stats/Stats"
+import StatsTable from "@/components/stats/Stats"
 
 export default function Statistics() {
   const { data: stats, isLoading, error } = useQuery<StatsTableType[], Error>({
