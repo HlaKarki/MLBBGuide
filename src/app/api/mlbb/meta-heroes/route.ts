12bfc999f2383ed628cda93027e29c1de8da3ac3
hlakarki <kshap.karki@gmail.com>
2024-10-24 11:43:27 -0400

    fix: Update fetchStats call to include pick_rate in processed data for hero statistics in the route handler.

--- src/app/api/mlbb/meta-heroes/route.ts
+++ src/app/api/mlbb/meta-heroes/route.ts
@@ -1,27 +1,33 @@
 // src/app/api/mlbb/meta-heroes/route.ts
-import {NextRequest, NextResponse} from "next/server";
-import {MetaHeroesQueryType} from "@/lib/types";
-import {fetchStats} from "@/app/api/mlbb/fetches";
+import { NextRequest, NextResponse } from 'next/server';
+import { MetaHeroesQueryType } from '@/lib/types';
+import { fetchStats } from '@/app/api/mlbb/fetches';
 
 export async function GET(request: NextRequest) {
-  const count = Number(request.nextUrl.searchParams.get("count")) || 5
-  const hero_id = Number(request.nextUrl.searchParams.get("id"))
+  const count = Number(request.nextUrl.searchParams.get('count')) || 5;
+  const hero_id = Number(request.nextUrl.searchParams.get('id'));
 
   try {
-    const data =  await fetchStats("main_hero_ban_rate", count, hero_id);
-    const processedData = data.data.records.map((record: MetaHeroesQueryType) => {
-      return {
-        ban_rate: record.data.main_hero_ban_rate,
-        win_rate: record.data.main_hero_win_rate,
-        heroid: record.data.main_heroid,
-        head: record.data.main_hero.data.head,
-        name: record.data.main_hero.data.name,
+    const data = await fetchStats('main_hero_ban_rate', count, hero_id);
+    const processedData = data.data.records.map(
+      (record: MetaHeroesQueryType) => {
+        return {
+          ban_rate: record.data.main_hero_ban_rate,
+          win_rate: record.data.main_hero_win_rate,
+          pick_rate: record.data.main_hero_appearance_rate,
+          heroid: record.data.main_heroid,
+          head: record.data.main_hero.data.head,
+          name: record.data.main_hero.data.name,
+        };
       }
-    })
+    );
 
     return NextResponse.json(processedData);
   } catch (error) {
     console.error('Error fetching hero data:', error);
-    return NextResponse.json({ error: 'Failed to fetch hero data' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Failed to fetch hero data' },
+      { status: 500 }
+    );
   }
 }
