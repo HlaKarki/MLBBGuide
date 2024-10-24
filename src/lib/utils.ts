c73fabd16ee0170c9ace6fbb98186f18d2cc9792
hlakarki <kshap.karki@gmail.com>
2024-10-24 11:19:44 -0400

    feat: Add functions to retrieve hero names and IDs from JSON data in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import jsonData from "@/lib/data/ids.json"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getHeroName(id: string | number): string {
+  return jsonData.heroes.find(hero => hero.id === Number(id))?.name || "unknown";
+}
+
+export function getHeroId(name: string) : number | undefined {
+  return jsonData.heroes.find(hero => hero.name === name)?.id || undefined
 }
