c54ae7c6ad906a0f1e5fa5d96a8e81b78dfe53cc
hlakarki <kshap.karki@gmail.com>
2024-10-31 17:23:51 -0400

    feat: Add firebase-admin initialization file for Firestore database connectivity

---
+++ src/lib/db/firebase-admin.ts
@@ -0,0 +1,18 @@
+// /lib/firebase-admin.ts
+import admin from "firebase-admin";
+import { getApps } from "firebase-admin/app";
+
+if (!getApps().length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+    databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  });
+}
+
+const db = admin.firestore();
+
+export { db };
