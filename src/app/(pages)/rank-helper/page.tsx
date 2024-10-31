ed39fe00384745259d41a03928bfe551f410aa83
hlakarki <kshap.karki@gmail.com>
2024-10-31 17:33:00 -0400

    feat: Implement data fetching and state management in RankHelper component for room data retrieval.

--- src/app/(pages)/rank-helper/page.tsx
+++ src/app/(pages)/rank-helper/page.tsx
@@ -1,7 +1,61 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { doc, getDoc } from '@firebase/firestore';
+import { db } from '@/lib/db/firebase';
+import { signIn } from '@/lib/db/sign-in';
+
 export default function RankHelper() {
+  const [data, setData] = useState<any>([]);
+
+  const enterRoom = useCallback(async () => {
+    try {
+      await signIn()
+      const roomDocRef = doc(db, 'test', 'xej6GQcXXv2q4KfMXbWj');
+      const docSnap = await getDoc(roomDocRef);
+
+      if (docSnap.exists()) {
+        console.log("fetched: ", docSnap.data());
+        setData(docSnap.data());
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    enterRoom().catch(console.error);
+  }, []);
+
   return (
-    <div className={'text-white'}>
+    <div className="text-white">
       <h1>Rank helper</h1>
     </div>
   );
 }
+
+// const query = async () => {
+//   const response = await fetch('/api/firebase/test')
+//   const data = await response.json();
+//   console.log("data: ", data);
+// }
+//
+// useEffect(() => {
+//   query().catch(console.error);
+// }, []);
+
+/*
+* context: {
+*   allies: {
+*     roam: ""
+*     fighter: "",
+*     mage: "",
+*     marksman: "",
+*     jungle: ""
+*   }
+* }
+* suggestedHeroes(context)
+*
+*
+*
+ */
