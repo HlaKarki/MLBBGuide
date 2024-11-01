4c9daed327e9d31d10c3c5276570a69d3202212f
hlakarki <kshap.karki@gmail.com>
2024-11-01 14:33:23 -0400

    delete: Remove unused 'route.ts' file for test API and clean up console logs in admin login API for improved code quality.

--- src/app/api/firebase/admin_login/route.ts
+++ src/app/api/firebase/admin_login/route.ts
@@ -7,12 +7,6 @@ export async function GET(request: NextRequest) {
   const email = request.nextUrl.searchParams.get("email");
   const password = request.nextUrl.searchParams.get("password");
 
-  console.log({
-    EMAIL,
-    PASSWORD,
-    email,
-    password
-  });
   if (email && password && (email === EMAIL && password === PASSWORD)) {
     return NextResponse.json({
       success: true,
