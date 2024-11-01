4d72f6c8ef9436e0ca0cd49b3649719f93a034b2
hlakarki <kshap.karki@gmail.com>
2024-11-01 10:03:42 -0400

    delete: Remove old Firebase admin initialization code and replace with new import statements for Firestore integration.

--- src/lib/db/firebase-admin.ts
+++ src/lib/db/firebase-admin.ts
@@ -1,26 +1,4 @@
 // /lib/db/firebase-admin.ts
-// import admin from "firebase-admin";
-// import { getApps } from "firebase-admin/app";
-//
-// if (!getApps().length) {
-//   try {
-//     admin.initializeApp({
-//       credential: admin.credential.cert({
-//         projectId: process.env.FIREBASE_PROJECT_ID,
-//         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-//       }),
-//     });
-//   } catch (error) {
-//     console.log('Firebase admin initialization error', error);
-//   }
-// }
-//
-// const db = admin.firestore();
-//
-// export { db };
-
-
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
