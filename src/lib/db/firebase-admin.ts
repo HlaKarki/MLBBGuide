67e94cb844d572776dd4d2ff8f43dc7260e5e58a
hlakarki <kshap.karki@gmail.com>
2024-10-31 21:06:08 -0400

    refactor: Comment out initialization check for Firebase apps in firebase-admin.ts to improve code clarity and prevent unnecessary initialization attempts.

--- src/lib/db/firebase-admin.ts
+++ src/lib/db/firebase-admin.ts
@@ -2,7 +2,7 @@
 import admin from "firebase-admin";
 import { getApps } from "firebase-admin/app";
 
-if (!getApps().length) {
+// if (!getApps().length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
@@ -14,15 +14,7 @@ if (!getApps().length) {
   } catch (error) {
     console.log('Firebase admin initialization error', error);
   }
-  // admin.initializeApp({
-  //   credential: admin.credential.cert({
-  //     projectId: process.env.FIREBASE_PROJECT_ID,
-  //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  //     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  //   }),
-  //   databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
-  // });
-}
+// }
 
 const db = admin.firestore();
 
