1d7d0823468dbd2ba0f3244337db5a4b36000f4e
hlakarki <kshap.karki@gmail.com>
2024-11-02 23:11:34 -0400

    feat: Added ThemeProvider component to manage theme context with NextThemes in providers.tsx.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { QueryClient, QueryClientProvider as TanstackQueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
+import { QueryClient, QueryClientProvider as TanstackQueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export default function QueryClientProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
@@ -11,4 +13,13 @@ export default function QueryClientProvider({ children }: { children: React.Reac
         {children}
       </TanstackQueryClientProvider>
   )
+}
+
+
+
+export function ThemeProvider({
+                                        children,
+                                        ...props
+                                      }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
