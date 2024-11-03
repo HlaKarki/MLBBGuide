a5bb221d610b7661cdf0c4a0d2d3d6b3974f16af
hlakarki <kshap.karki@gmail.com>
2024-11-02 23:11:14 -0400

    feat: Implement loading and error handling in RankHelper component and add RankedSidebar for improved UI layout.

--- src/app/(pages)/rank-helper/page.tsx
+++ src/app/(pages)/rank-helper/page.tsx
@@ -1,9 +1,44 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+import { FinalHeroDataType } from '@/lib/types';
+import { RankedSidebar } from '@/app/(pages)/rank-helper/Sidebar';
+import { getHeroNameURL } from '@/lib/utils';
+
 export default function RankHelper() {
+  const { data, isLoading, error, isError } = useQuery<FinalHeroDataType[]>({
+    queryKey: [],
+    queryFn: async () => {
+      const response = await fetch('/api/mlbb/final')
+      const heroes = await response.json()
+      return heroes.data as FinalHeroDataType[]
+    },
+  });
+
   return (
-    <div className="text-white">
-      <h1>Rank helper</h1>
+    <div className="text-white flex">
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{error.message}</div>}
+      <RankedSidebar />
+      {data && (
+        <div className={'flex flex-wrap gap-4'}>
+          {data.map((hero, index) => (
+            <a
+              key={hero.hero_id}
+              className={
+                'flex flex-col justify-center items-start truncate w-[80px]'
+              }
+              href={`/search#${getHeroNameURL(hero.hero_id)}`}
+            >
+              <img
+                className={'h-20 w-20 rounded-full'}
+                src={hero.images.head}
+              />
+              <h3 className={'flex mx-auto'}>{hero.name}</h3>
+            </a>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
@@ -16,10 +51,29 @@ export default function RankHelper() {
  *     mage: "",
  *     marksman: "",
  *     jungle: ""
- *   }
+ *   },
+ * enemies: {
+ *     roam: ""
+ *     fighter: "",
+ *     mage: "",
+ *     marksman: "",
+ *     jungle: ""
+ *  }
  * }
  * suggestedHeroes(context)
  *
  *
  *
  */
+
+
+// const heroesCollectionRef = collection(db, 'heroes');
+// const rolesToFind = ['Exp Lane', 'Jungle'];
+// const heroesQuery = query(
+//   heroesCollectionRef,
+//   where('role', 'array-contains-any', rolesToFind)
+// );
+// const querySnapshot = await getDocs(heroesQuery);
+// const heroes = querySnapshot.docs.map(doc => ({
+//   ...doc.data(),
+// })) as FinalHeroDataType[];
