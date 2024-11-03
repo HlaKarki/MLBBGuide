7895d7ab65b1440a4e13633d5071d970786d49e2
hlakarki <kshap.karki@gmail.com>
2024-11-02 23:34:28 -0400

    feat: Implement filter functionality in RankHelper component and update RankedSidebar to handle lane changes for hero data display.

--- src/app/(pages)/rank-helper/page.tsx
+++ src/app/(pages)/rank-helper/page.tsx
@@ -4,8 +4,12 @@ import { useQuery } from '@tanstack/react-query';
 import { FinalHeroDataType } from '@/lib/types';
 import { RankedSidebar } from '@/app/(pages)/rank-helper/Sidebar';
 import { getHeroNameURL } from '@/lib/utils';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function RankHelper() {
+  const [filter, setFilter] = useState<{lane: string} | null>(null);
+  const [filteredData, setFilteredData] = useState<FinalHeroDataType[]>([]);
+
   const { data, isLoading, error, isError } = useQuery<FinalHeroDataType[]>({
     queryKey: [],
     queryFn: async () => {
@@ -15,30 +19,64 @@ export default function RankHelper() {
     },
   });
 
+  const filterData = useCallback(() => {
+      if (!data) return;
+
+      const filtered =  data.filter(hero => {
+        return hero.role.includes(filter?.lane || '')
+      })
+    setFilteredData(filtered);
+
+  }, [filter])
+
+  useEffect(() => {
+    filterData()
+  }, [filter]);
+
   return (
     <div className="text-white flex">
       {isLoading && <div>Loading...</div>}
       {isError && <div>{error.message}</div>}
-      <RankedSidebar />
-      {data && (
-        <div className={'flex flex-wrap gap-4'}>
-          {data.map((hero, index) => (
-            <a
-              key={hero.hero_id}
-              className={
-                'flex flex-col justify-center items-start truncate w-[80px]'
-              }
-              href={`/search#${getHeroNameURL(hero.hero_id)}`}
-            >
-              <img
-                className={'h-20 w-20 rounded-full'}
-                src={hero.images.head}
-              />
-              <h3 className={'flex mx-auto'}>{hero.name}</h3>
-            </a>
-          ))}
-        </div>
-      )}
+      <RankedSidebar onChange={(obj: {lane: string}) => setFilter(obj)} />
+      {
+        filteredData && filteredData.length > 0 ? (
+          <div className={'flex flex-wrap gap-4'}>
+            {filteredData.map((hero, index) => (
+              <a
+                key={hero.hero_id}
+                className={
+                  'flex flex-col justify-center items-start truncate w-[80px]'
+                }
+                href={`/search#${getHeroNameURL(hero.hero_id)}`}
+              >
+                <img
+                  className={'h-20 w-20 rounded-full'}
+                  src={hero.images.head}
+                />
+                <h3 className={'flex mx-auto'}>{hero.name}</h3>
+              </a>
+            ))}
+          </div>
+        ) : (
+            <div className={'flex flex-wrap gap-4'}>
+              {data && data.map((hero, index) => (
+                <a
+                  key={hero.hero_id}
+                  className={
+                    'flex flex-col justify-center items-start truncate w-[80px]'
+                  }
+                  href={`/search#${getHeroNameURL(hero.hero_id)}`}
+                >
+                  <img
+                    className={'h-20 w-20 rounded-full'}
+                    src={hero.images.head}
+                  />
+                  <h3 className={'flex mx-auto'}>{hero.name}</h3>
+                </a>
+              ))}
+            </div>
+        )
+      }
     </div>
   );
 }
