3a729f77e94427da9a1e213f0fdcd8454f93b93e
hlakarki <kshap.karki@gmail.com>
2024-11-04 11:47:28 -0500

    feat: Enhance header by adding user avatar popover with logout functionality and redirect on sign-in button.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import Link from 'next/link';
 import React from 'react';
 import { Logo } from '@/components/Logo';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, SignInButton, useClerk, useUser } from '@clerk/nextjs';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const { user } = useUser();
+  const { signOut } = useClerk();
+  const pathname = usePathname();
+
   return (
     <header className="text-white p-4">
       <div className="flex justify-start items-center gap-8">
@@ -34,10 +43,20 @@ export default function Header() {
         </nav>
         <span className={'flex-grow flex justify-end'}>
           <SignedOut>
-            <SignInButton mode={"modal"}/>
+            <SignInButton mode={"modal"} forceRedirectUrl={pathname} />
           </SignedOut>
           <SignedIn>
-            <UserButton />
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="ghost" className="p-0 rounded-full">
+                  <img src={user?.imageUrl} alt="Avatar" className="w-8 h-8 rounded-full" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className={"max-w-[200px] flex flex-col gap-2"}>
+                <Button>Logged Matches</Button>
+                <Button onClick={() => signOut({redirectUrl: pathname})}>Log Out</Button>
+              </PopoverContent>
+            </Popover>
           </SignedIn>
         </span>
       </div>
