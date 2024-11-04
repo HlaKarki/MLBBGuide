b25b37081e02c49b1941894ae51a5b42c3117715
hlakarki <kshap.karki@gmail.com>
2024-11-04 12:22:42 -0500

    feat: Implement user check function with Firebase integration in rank-helper helpers.tsx file.

---
+++ src/app/(pages)/rank-helper/helpers.tsx
@@ -0,0 +1,33 @@
+import { collection, getDocs, query, where } from '@firebase/firestore';
+import { db } from '@/lib/db/firebase';
+
+export async function checkUser(user: any){
+  const usersRef = collection(db, "users")
+  const queryRef = query(usersRef, where("clerk_id", "==", user.id))
+
+  try {
+    // Execute the query and get the documents
+    const querySnapshot = await getDocs(queryRef);
+
+    // Check if no user was found
+    if (querySnapshot.empty) {
+      // Add the new user to the database
+      const newUser = {
+        clerk_id: user.id,
+        username: user.username,
+        email: user.primaryEmailAddress.emailAddress,
+      };
+
+      const response = await fetch('/api/firebase/add_user', {
+        method: 'POST',
+        body: JSON.stringify(newUser),
+      })
+      const data = await response.json()
+
+      if (!data.success) console.error('Error adding user to the database');
+    }
+  } catch (error) {
+    console.error('Error checking user:', error);
+    throw new Error('Failed to check user');
+  }
+}
