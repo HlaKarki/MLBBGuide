eaed6bcdc292d02a925040fcf7c344ca5c631947
hlakarki <kshap.karki@gmail.com>
2024-11-04 12:34:15 -0500

    feat: Add user sign-in status display to RankHelper component with conditional rendering of username or sign-in message.

--- src/app/(pages)/rank-helper/page.tsx
+++ src/app/(pages)/rank-helper/page.tsx
@@ -1,14 +1,33 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
+import { useUser } from '@clerk/nextjs';
+import { checkUser } from '@/app/(pages)/rank-helper/helpers';
 
 export default function RankHelper() {
-  const { data, isLoading, error, isError } = useQuery({
-    queryKey: [''],
+  const {user, isSignedIn} = useUser();
+
+  const { data: userData, isLoading, error, isError } = useQuery({
+    queryKey: ['username', user?.username],
     queryFn: async () => {
-      // fetch all the game sessions
+      return await checkUser(user).catch(console.error);
     },
+    enabled: !!isSignedIn,
   });
 
-  return <div>Rank Helper</div>;
+  return (
+    <div>
+      <h1>Rank Helper</h1>
+      { isSignedIn ? (
+        <div>
+          {userData && userData.user?.username}
+        </div>
+      ) : (
+        <div>
+          Not Signed In
+        </div>
+      )
+      }
+    </div>
+  )
 }
