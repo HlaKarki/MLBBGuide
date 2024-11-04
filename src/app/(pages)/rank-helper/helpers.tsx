984ae7786b38cdf97a3f8a18a565c071493ccc2f
hlakarki <kshap.karki@gmail.com>
2024-11-04 12:34:08 -0500

    fix: Enhance error handling in checkUser function and return user data structure for successful queries.

--- src/app/(pages)/rank-helper/helpers.tsx
+++ src/app/(pages)/rank-helper/helpers.tsx
@@ -1,4 +1,4 @@
-import { collection, getDocs, query, where } from '@firebase/firestore';
+import { collection, DocumentData, getDocs, query, where } from '@firebase/firestore';
 import { db } from '@/lib/db/firebase';
 
 export async function checkUser(user: any){
@@ -24,8 +24,17 @@ export async function checkUser(user: any){
       })
       const data = await response.json()
 
-      if (!data.success) console.error('Error adding user to the database');
+      if (!data.success) {
+        console.error('Error adding user to the database');
+        return { success: false, user: undefined };
+      }
+
+      return { success: true, user: newUser };
     }
+
+    const foundUser: DocumentData = querySnapshot.docs[0].data();
+    return { success: true, user: foundUser }
+
   } catch (error) {
     console.error('Error checking user:', error);
     throw new Error('Failed to check user');
