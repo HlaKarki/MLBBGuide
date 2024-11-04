c6b7a088f2fdfe64b51a5ba9b20a2ea8d11b4d8f
hlakarki <kshap.karki@gmail.com>
2024-11-04 14:11:15 -0500

    feat: Implement toggle functionality for 'Create New Game' button and integrate CreateGameFlow component in Games page.

--- src/app/(pages)/rank-helper/games.tsx
+++ src/app/(pages)/rank-helper/games.tsx
@@ -1,13 +1,18 @@
 import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/card';
 import { UserDataType } from '@/lib/types';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import { CreateGameFlow } from '@/app/(pages)/rank-helper/CreateGameFlow';
 
 export function Games({userData} : {userData: UserDataType | undefined}) {
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
   return (
     <div>
-      <Button>
+      <Button onClick={() => setDialogOpen(prev => !prev)}>
         Create New Game
       </Button>
+      <CreateGameFlow dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} />
+
       {userData && userData.games && userData.games.map((data: any, idx: number) => {
         return (
           <Card key={"idx"}>
