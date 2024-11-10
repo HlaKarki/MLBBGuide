a178f188b7b86c81cb4a45d15d13d116a16f37aa
hlakarki <kshap.karki@gmail.com>
2024-11-09 22:02:29 -0500

    feat: Enhance API route by adding POST method for fetching data based on hero_id and rank, improving data retrieval flexibility.

--- src/app/api/mlbb/final/route.ts
+++ src/app/api/mlbb/final/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { fetchMLBBData } from '@/lib/fetches';
 
 export async function GET() {
   try {
-    const response = await fetchMLBBData()
+    const response = await fetchMLBBData(undefined, undefined)
 
     return NextResponse.json({
       data: response,
@@ -15,5 +15,23 @@ export async function GET() {
       message: "Failed to fetch final data"
       }
     )
+  }
+}
+
+export async function POST(request: NextRequest){
+  const { hero_id, rank } = await request.json();
+
+  try {
+    const response = await fetchMLBBData(hero_id, rank)
+
+    return NextResponse.json({
+      data: response,
+      success: true,
+    })
+  } catch(error) {
+    NextResponse.json({
+      success: false,
+      message: "Failed to fetch final data (single)",
+    })
   }
 }
