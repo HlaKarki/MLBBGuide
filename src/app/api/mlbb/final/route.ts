3bfd80f861414a579001cc8163a9fd98925bf2ac
hlakarki <kshap.karki@gmail.com>
2024-11-10 14:18:36 -0500

    refactor: Modify GET and POST functions to accept parameters from NextRequest for improved data fetching in route.ts

--- src/app/api/mlbb/final/route.ts
+++ src/app/api/mlbb/final/route.ts
@@ -1,37 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { fetchMLBBData } from '@/lib/fetches';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const hero_id = request.nextUrl.searchParams.get('hero_id');
+  const rank = request.nextUrl.searchParams.get('rank');
+
   try {
-    const response = await fetchMLBBData(undefined, undefined)
+    const response = await fetchMLBBData(
+      hero_id || undefined,
+      rank || undefined
+    );
 
     return NextResponse.json({
       data: response,
       success: true,
-    })
-  } catch(error) {
+    });
+  } catch (error) {
     NextResponse.json({
       success: false,
-      message: "Failed to fetch final data"
-      }
-    )
+      message: 'Failed to fetch final data',
+    });
   }
 }
 
-export async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
   const { hero_id, rank } = await request.json();
 
   try {
-    const response = await fetchMLBBData(hero_id, rank)
+    const response = await fetchMLBBData(hero_id, rank);
 
     return NextResponse.json({
       data: response,
       success: true,
-    })
-  } catch(error) {
+    });
+  } catch (error) {
     NextResponse.json({
       success: false,
-      message: "Failed to fetch final data (single)",
-    })
+      message: 'Failed to fetch final data (single)',
+    });
   }
 }
