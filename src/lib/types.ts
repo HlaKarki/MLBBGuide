d16437723682106a5f8456720988baf16794d487
hlakarki <kshap.karki@gmail.com>
2024-11-09 22:01:40 -0500

    feat: Added API request and response type interfaces for Hero, Counter, Meta, and Graph data in types.ts to enhance data handling and structure.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -210,3 +210,107 @@ export type UserDataType = {
 
 // Ranks
 export type RanksType = "Warrior" | "Elite" | "Grandmaster" | "Epic" | "Legend" | "Mythical Honor" | "Mythical Glory" | "All";
+
+export interface APIRequestConfig {
+  pageSize: number;
+  filters?: Array<{
+    field: string;
+    operator: string;
+    value: string | number;
+  }>;
+  sorts: Array<{
+    data: {
+      field: string;
+      order: 'asc' | 'desc';
+    };
+    type: 'sequence';
+  }>;
+  pageIndex: number;
+  fields: string[];
+  object?: any[];
+}
+
+
+// Define interfaces for API responses
+export interface HeroDataAPIResponse {
+  data: {
+    records: {
+      data: {
+        hero_id: string;
+        head: string;
+        head_big: string;
+        hero: {
+          data: {
+            name: string;
+            squarehead: string;
+            squareheadbig: string;
+            speciality: string[];
+            abilityshow: string[];
+            roadsortlabel: string[];
+            story: string;
+          };
+        };
+      };
+    }[];
+  };
+}
+
+export interface CounterDataAPIResponse {
+  data: {
+    records: {
+      data: {
+        main_heroid: string;
+        sub_hero: {
+          hero: {
+            data: {
+              head: string;
+            };
+          };
+          heroid: string;
+          increase_win_rate: string;
+        }[];
+        sub_hero_last: {
+          hero: {
+            data: {
+              head: string;
+            };
+          };
+          heroid: string;
+          increase_win_rate: string;
+        }[];
+      };
+    }[];
+  };
+}
+
+export interface MetaDataAPIResponse {
+  data: {
+    records: {
+      data: {
+        main_heroid: string;
+        main_hero_win_rate: string;
+        main_hero_ban_rate: string;
+        main_hero_appearance_rate: string;
+      };
+    }[];
+  };
+}
+
+export interface GraphDataAPIResponse {
+  data: {
+    records: {
+      _createdAt: number;
+      _updatedAt: number;
+      data: {
+        bigrank: string;
+        main_heroid: string;
+        win_rate: {
+          app_rate: number;
+          ban_rate: number;
+          win_rate: number;
+          date: string;
+        }[];
+      };
+    }[];
+  };
+}
