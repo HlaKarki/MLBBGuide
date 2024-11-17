fbdd8d82eef931fb0bab8687c599c5c22da15c5d
hlakarki <kshap.karki@gmail.com>
2024-11-17 14:16:54 -0500

    feat: Add fetchLore function to retrieve lore data from Firebase based on hero_id.

--- src/lib/fetches.ts
+++ src/lib/fetches.ts
@@ -3,6 +3,7 @@ import {
 } from '@/lib/types';
 import { MLBBApiClient } from '@/lib/api/mlbb/client';
 import { DataProcessor } from '@/lib/api/mlbb/processors';
+import { db } from '@/lib/db/firebase-admin';
 
 export async function fetchMLBBData(hero_id: string | undefined, rank: string | undefined): Promise<FinalHeroDataType[]> {
   const rawData = await MLBBApiClient.fetchAllData(rank, hero_id);
@@ -22,4 +23,15 @@ export async function fetchMLBBData(hero_id: string | undefined, rank: string | 
     processedData.meta,
     processedData.graph
   );
+}
+
+export async function fetchLore(hero_id: string) {
+  const docRef = db.collection("lores").doc(hero_id);
+  const docSnap = await docRef.get();
+  if (docSnap.exists) {
+    const data = docSnap.data();
+    return { success: true, data: data };
+  } else {
+    return { success: false, data: null, message: "Document not found" }
+  }
 }
