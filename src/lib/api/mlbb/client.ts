5f732593d4b3301638dd12481b789dfe8ace48b7
hlakarki <kshap.karki@gmail.com>
2024-11-19 11:16:15 -0500

    refactor: Improve formatting of async fetchData method and related calls in MLBBApiClient for better readability and maintainability.

--- src/lib/api/mlbb/client.ts
+++ src/lib/api/mlbb/client.ts
@@ -9,7 +9,10 @@ import { API_CONFIG, RequestBody } from '@/config/api';
 import { RequestBodyFactory } from '@/lib/api/mlbb/builder';
 
 export class MLBBApiClient {
-  private static async fetchData<T>(url: string, body: RequestBody): Promise<T> {
+  private static async fetchData<T>(
+    url: string,
+    body: RequestBody
+  ): Promise<T> {
     const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -26,11 +29,26 @@ export class MLBBApiClient {
   static async fetchAllData(rank: string = 'All', heroId?: string) {
     try {
       const [heroData, counters, compatibles, meta, graph] = await Promise.all([
-        this.fetchData<HeroDataAPIResponse>(API_CONFIG.endpoints.heroes, RequestBodyFactory.createHeroDataRequest(heroId)),
-        this.fetchData<CounterDataAPIResponse>(API_CONFIG.endpoints.details, RequestBodyFactory.createMatchupRequest('counter', rank, heroId)),
-        this.fetchData<CounterDataAPIResponse>(API_CONFIG.endpoints.details, RequestBodyFactory.createMatchupRequest('compatible', rank, heroId)),
-        this.fetchData<MetaDataAPIResponse>(API_CONFIG.endpoints.meta, RequestBodyFactory.createMetaRequest(rank, heroId)),
-        this.fetchData<GraphDataAPIResponse>(API_CONFIG.endpoints.graph, RequestBodyFactory.createGraphRequest(rank, heroId)),
+        this.fetchData<HeroDataAPIResponse>(
+          API_CONFIG.endpoints.heroes,
+          RequestBodyFactory.createHeroDataRequest(heroId)
+        ),
+        this.fetchData<CounterDataAPIResponse>(
+          API_CONFIG.endpoints.details,
+          RequestBodyFactory.createMatchupRequest('counter', rank, heroId)
+        ),
+        this.fetchData<CounterDataAPIResponse>(
+          API_CONFIG.endpoints.details,
+          RequestBodyFactory.createMatchupRequest('compatible', rank, heroId)
+        ),
+        this.fetchData<MetaDataAPIResponse>(
+          API_CONFIG.endpoints.meta,
+          RequestBodyFactory.createMetaRequest(rank, heroId)
+        ),
+        this.fetchData<GraphDataAPIResponse>(
+          API_CONFIG.endpoints.graph,
+          RequestBodyFactory.createGraphRequest(rank, heroId)
+        ),
       ]);
 
       return {
@@ -46,9 +64,12 @@ export class MLBBApiClient {
     }
   }
 
-  static async fetchMicroHeroData(heroes: string[]){
+  static async fetchMicroHeroData() {
     try {
-      const response: MetaDataAPIResponse = await this.fetchData(API_CONFIG.endpoints.meta, RequestBodyFactory.createMetaRequest('Overall', '30', true))
+      const response: MetaDataAPIResponse = await this.fetchData(
+        API_CONFIG.endpoints.meta,
+        RequestBodyFactory.createMetaRequest('Overall', undefined, true)
+      );
       return {
         data: response,
       };
