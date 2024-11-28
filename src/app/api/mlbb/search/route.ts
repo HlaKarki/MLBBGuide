b9fd86bbde315cbb905b664fe58f6c97164d862d
hlakarki <kshap.karki@gmail.com>
2024-11-27 20:16:53 -0500

    fix: Update GET function to accept request parameters for rank, hero_count, and stat_type in search route.

--- src/app/api/mlbb/search/route.ts
+++ src/app/api/mlbb/search/route.ts
@@ -1,9 +1,18 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { fetchMicroHeroData } from '@/lib/fetches';
+import { MetaStatsType } from '@/lib/types';
 
-export async function GET() {
+export const fetchCache = 'force-no-store';
+
+export async function GET(request: NextRequest) {
+  const rank = request.nextUrl.searchParams.get('rank')!;
+  const hero_count = Number(request.nextUrl.searchParams.get('hero_count'));
+  const stat_type = request.nextUrl.searchParams.get(
+    'stat_type'
+  ) as MetaStatsType;
+
   try {
-    const response = await fetchMicroHeroData();
+    const response = await fetchMicroHeroData(rank, stat_type, hero_count);
     return NextResponse.json({
       data: response.reverse(),
       success: true,
